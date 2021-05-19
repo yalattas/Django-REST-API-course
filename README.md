ba690a503fd35276bdf8c2cd819206bc22e04d97
Yasser Alattas <38978501+yalattas@users.noreply.github.com>
2021-05-20 02:20:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django-REST-API-course
+ Python Django REST API
---
+++ apisApp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post (models.Model):
+    title = models.CharField(
+        max_length=100
+    )
+    url = models.URLField()
+    # Require User model to be imported
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    # To take current time and submit it
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # To list by the recent posts
+        ordering = ['-created_at']
+
+class Vote(models.Model):
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
